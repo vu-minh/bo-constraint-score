26d9a1abbdf60c9f74e868d8ab6caf71e4805e6f
vu-minh <vuvietminh@unamur.be>
2019-07-31 09:02:52 +0200

    repeat run score and plot mean score with std

--- code/run_score.py
+++ code/run_score.py
@@ -9,27 +9,90 @@ import matplotlib.pyplot as plt
 from common.dataset import dataset
 import utils
 
-# nested_dict = lambda: collections.defaultdict(nested_dict)
 
-
-def run_score(method_name, score_name, list_n_labels_values, seed=None, degrees_of_freedom=1.0):
+def run_score(method_name, score_name, list_n_labels_values,
+              seed=42, n_repeat=1, degrees_of_freedom=1.0):
     all_embeddings = joblib.load(f"{embedding_dir}/all.z")
-    scores = collections.defaultdict(dict)
 
-    for n_labels_each_class in list_n_labels_values:
-        constraints = utils.generate_constraints(
-            constraint_strategy="partial_labels", score_name=score_name,
-            labels=labels, seed=seed,
-            n_labels_each_class=n_labels_each_class)
+    for i in range(n_repeat):
+        print("[Debug] n_run: ", i + 1)
+        scores = collections.defaultdict(dict)
+        run_index = seed + i
 
-        for param_value, embedding in enumerate(all_embeddings):
-            if embedding is not None:
-                score = utils.score_embedding(embedding, score_name, constraints,
-                                              degrees_of_freedom=degrees_of_freedom)
-                scores[n_labels_each_class][param_value] = score
+        for n_labels_each_class in list_n_labels_values:
+            constraints = utils.generate_constraints(
+                constraint_strategy="partial_labels", score_name=score_name,
+                labels=labels, seed=run_index,
+                n_labels_each_class=n_labels_each_class)
 
-    with open(f"{score_dir}/raw_scores_dof{degrees_of_freedom}.txt", "w") as out_file:
-        json.dump(scores, out_file)
+            for param_value, embedding in enumerate(all_embeddings):
+                if embedding is not None:
+                    score = utils.score_embedding(embedding, score_name, constraints,
+                                                  degrees_of_freedom=degrees_of_freedom)
+                    scores[n_labels_each_class][param_value] = score
+
+        out_name = f"{score_dir}/dof{degrees_of_freedom}_{run_index}.txt"
+        with open(out_name, "w") as out_file:
+            json.dump(scores, out_file)
+
+    merge_all_score_files(method_name, score_name, list_n_labels_values, seed=seed,
+                          n_repeat=n_repeat, degree_of_freedom=degree_of_freedom)
+
+
+def merge_all_score_files(method_name, score_name, list_n_labels_values,
+                          seed=42, n_repeat=1, degrees_of_freedom=1.0):
+    all_scores = collections.defaultdict(list)
+    list_params = None
+    
+    for i in range(n_repeat):
+        run_index = seed + i
+        in_name = f"{score_dir}/dof{degrees_of_freedom}_{run_index}.txt"
+        with open(in_name, "r") as in_file:
+            scores_run_i = json.load(in_file)
+
+        for n_labels_each_class, scores_i_j in scores_run_i.items():
+            if list_params is None:
+                list_params = list(scores_i_j.keys())
+            elif list_params != list(scores_i_j.keys()):
+                raise ValueError(f"Inconsistant list params in score files: {in_name}")
+
+            all_scores[n_labels_each_class].append(list(scores_i_j.values()))
+
+    print(len(list_params))
+    for n_labels_each_class, scores_i_j in all_scores.items():
+        print(n_labels_each_class, len(scores_i_j), len(scores_i_j[0]))
+
+    with open(f"{score_dir}/dof{degrees_of_freedom}_all.txt", "w") as out_file:
+        json.dump({
+            'list_params': list_params,
+            'all_scores': all_scores
+        }, out_file)
+
+
+def plot_scores_with_std(method_name, score_name, list_n_labels_values,
+                         seed=42, n_repeat=1, degrees_of_freedom=1.0):
+    with open(f"{score_dir}/dof{degrees_of_freedom}_all.txt", "r") as in_file:
+        json_data = json.load(in_file)
+
+    list_params = json_data['list_params']
+    all_scores = json_data['all_scores']
+
+    def _plot_scores_with_variance(ax, scores):
+        mean = np.mean(scores, axis=0)
+        sigma = np.std(scores, axis=0)
+        ax.plot(mean)
+        ax.fill_between(np.array(list_params), mean + 2 * sigma, mean - 2 * sigma,
+                        fc="#CCDAF1", alpha=0.5)
+        ax.axvline(np.argmax(mean), color='g', linestyle='--', alpha=0.5)
+
+    n_rows = len(all_scores)
+    _, axes = plt.subplots(n_rows, 1, figsize=(16, 4*n_rows))
+    for ax, (n_labels_each_class, scores) in zip(np.array(axes).ravel(), all_scores.items()):
+        print(n_labels_each_class, len(scores))
+        _plot_scores_with_variance(ax, np.array(scores))
+        # ax.set_title(f"{n_labels_each_class} labels each class")
+    plt.savefig(f"{plot_dir}/scores_with_std_dof{degrees_of_freedom}.png")
+    plt.savefig("temp.png")
 
 
 def plot_score(method_name, score_name, list_n_labels_values, degrees_of_freedom=1.0):
@@ -58,6 +121,8 @@ if __name__ == "__main__":
                     help="['tsne', 'umap', 'largevis', 'TODO-umap-n-params']")
     ap.add_argument("-sc", "--score_name", default="qij",
                     help=" in ['qij', 'contrastive', 'cosine']")
+    ap.add_argument("-nr", "--n_repeat", default=1, type=int,
+                    help="number of times to repeat the score calculation")
     ap.add_argument("-nl", "--n_labels_each_class", default=5, type=int,
                     help="number of labelled points selected for each class")
     ap.add_argument("-dof", "--degrees_of_freedom", default=1.0, type=float,
@@ -85,9 +150,15 @@ if __name__ == "__main__":
     list_n_labels_values = [5] if args.debug else range(2, 16)
 
     if args.run:
-        run_score(method_name, score_name, list_n_labels_values,
-                  seed=args.seed, degrees_of_freedom=args.degrees_of_freedom)
+        run_score(method_name, score_name, list_n_labels_values, seed=args.seed,
+                  n_repeat=args.n_repeat, degrees_of_freedom=args.degrees_of_freedom)
+        merge_all_score_files(method_name, score_name, list_n_labels_values, seed=args.seed,
+                              n_repeat=args.n_repeat,
+                              degrees_of_freedom=args.degrees_of_freedom)
 
     if args.plot:
-        plot_score(method_name, score_dir, list_n_labels_values,
-                   degrees_of_freedom=args.degrees_of_freedom)
+        # plot_score(method_name, score_dir, list_n_labels_values,
+        #            degrees_of_freedom=args.degrees_of_freedom)
+        plot_scores_with_std(method_name, score_name, list_n_labels_values, seed=args.seed,
+                             n_repeat=args.n_repeat,
+                             degrees_of_freedom=args.degrees_of_freedom)
