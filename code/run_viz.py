bc4dd0a95e1dbaf718323c037e284f1880937e44
vu-minh <vuvietminh@unamur.be>
2019-08-01 11:11:08 +0200

    refactor run different method and merge the embeddings

--- code/run_viz.py
+++ code/run_viz.py
@@ -2,6 +2,7 @@
 
 import os
 import joblib
+from itertools import product
 from subprocess import check_call
 
 import numpy as np
@@ -21,10 +22,18 @@ def run_viz(method_name, X, param_ranges, seed=42):
     }[method_name]()
 
 
-def run_largevis(X, perplexity_range, seed=42):
+def run_largevis(X, perplexity=30, seed=42, check_log=True, embedding_dir=""):
     # https://github.com/lferry007/LargeVis
 
+    print(f"[Debug] LargeVis(perplexity={perplexity}, seed={seed}")
+
+    embedded_file_name = f"{embedding_dir}/{perplexity}.z"
+    if check_log and os.path.exists(embedded_file_name):
+        print(f"[Debug] Reuse {embedded_file_name}")
+        return joblib.load(embedded_file_name)
+
     # perpare input file
+    current_path = os.path.dirname(os.path.realpath(__file__))
     input_file_name = f"{current_path}/temp_largevis_input.txt"
     input_header = f"{X.shape[0]} {X.shape[1]}"
     np.savetxt(input_file_name, X, header=input_header, comments='')  # disable comment header
@@ -33,79 +42,109 @@ def run_largevis(X, perplexity_range, seed=42):
     output_file_name = f"{current_path}/temp_largevis_output.txt"
     largevis_exe = "/opt/LargeVis/LargeVis_run.py"
 
-    for perp in perplexity_range:
-        print("Largevis with perplexity: ", perp)
+    check_call([
+        "python2", largevis_exe,
+        "-input", input_file_name,
+        "-output", output_file_name,
+        "-perp", f"{perplexity}",
+        "-samples", f"{int(X.shape[0]/100)}",
+        "-threads", "32"  # update number of threads for running on dev server
+    ])
 
-        check_call([
-            "python2", largevis_exe,
-            "-input", input_file_name,
-            "-output", output_file_name,
-            "-perp", f"{perp}",
-            "-samples", f"{int(X.shape[0]/100)}",
-            "-threads", "32"  # update number of threads for running on dev server
-        ])
-
-        # loaf from output file to numpy array
-        Z = np.loadtxt(output_file_name, skiprows=1)
-        print(Z.shape)
-
-        # save numpy array to z file
-        joblib.dump(Z, f"{embedding_dir}/{perp}.z")
+    # loaf from output file to numpy array
+    Z = np.loadtxt(output_file_name, skiprows=1)
+    # save numpy array to z file
+    joblib.dump(Z, embedded_file_name)
 
 
-def run_tsne(X, perplexity_range, seed=42):
-    for perp in perplexity_range:
-        print("Compute tsne with perp: ", perp)
-        Z = MulticoreTSNE(
-            perplexity=perp, n_iter=1500, n_jobs=2, random_state=seed,
-            n_iter_without_progress=1500, min_grad_norm=1e-32,
-        ).fit_transform(X)
-        joblib.dump(Z, f"{embedding_dir}/{perp}.z")
+def run_tsne(X, perplexity=30, seed=42, check_log=True, embedding_dir=""):
+    print(f"[Debug] MulticoreTSNE(perplexity={perplexity}, seed={seed})")
+
+    embedded_file_name = f"{embedding_dir}/{perplexity}.z"
+    if check_log and os.path.exists(embedded_file_name):
+        print(f"[Debug] Reuse {embedded_file_name}")
+        return joblib.load(embedded_file_name)
+
+    Z = MulticoreTSNE(
+        perplexity=perplexity, n_iter=1500, n_jobs=2, random_state=seed,
+        n_iter_without_progress=1500, min_grad_norm=1e-32,
+    ).fit_transform(X)
+    joblib.dump(Z, embedded_file_name)
 
 
-def run_umap(X, n_neighbors_range, seed=42):
-    for n_neighbors in n_neighbors_range:
-        print("Compute umap with n_neighbors: ", n_neighbors)
-        Z = umap.UMAP(
-            n_neighbors=n_neighbors, random_state=seed
-        ).fit_transform(X)
-        joblib.dump(Z, f"{embedding_dir}/{n_neighbors}.z")
+def run_umap(X, n_neighbors=15, min_dist=0.1, seed=42, check_log=True, embedding_dir=""):
+    print(f"[Debug] UMAP(n_neighbors={n_neighbors}, min_dist={min_dist}, seed={seed})")
+
+    embedded_file_name = f"{embedding_dir}/{n_neighbors}_{min_dist}.z"
+    if check_log and os.path.exists(embedded_file_name):
+        print(f"[Debug] Reuse {embedded_file_name}")
+        return joblib.load(embedded_file_name)
+
+    Z = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, random_state=seed) \
+            .fit_transform(X)
+    joblib.dump(Z, embedded_file_name)
 
 
-def merge_embeddings(n_embeddings):
-    all_embeddings = []
-    # load all pre-calculated embeddings
-    for i in range(n_embeddings):
-        log_name = f"{embedding_dir}/{i}.z"
-        all_embeddings.append(None if not os.path.exists(log_name)
-                              else joblib.load(log_name))
+def merge_embeddings(method_name, **kwargs):
+    """Merge all embeddings in .z file into a big .z file.
+
+    Each embedding is indexed by its param(s) in `kwargs`.
+    + tSNE and LargeVis use `perplexity`,
+    + UMAP use `n_neighbors` and `min_dist`
+   
+    Args:
+        method_name: name of method, in ['tsne', 'umap', 'largevis']
+        kwargs: keyword arguments for each method:
+                {'perplexity_range': []} for 'tsne' and 'largevis'
+                {'n_neighbors_range': [], 'min_dist_range': []} for 'umap'
+    Returns:
+        dict of all embeddings indexed by its corresponding param value(s)
+        E.g.: {50: []} or {50_0.1: []}
+    """
+    # check if kargs containts the correct param key
+    param_keys = {
+        'tsne': ['perplexity_range'],
+        'largevis': ['perplexity_range'],
+        'umap': ['n_neighbors_range', 'min_dist_range']
+    }
+    for param_key in param_keys[method_name]:
+        assert param_key in kwargs
+
+    # build list of embedding indices that will be used as file name
+    embedding_indices = []
+    if method_name in ['tsne', 'largevis']:
+        for perplexity in kwargs['perplexity_range']:
+            embedding_indices.append(str(perplexity))
+    if method_name in ['umap']:
+        for n_neighbors in kwargs['n_neighbors_range']:
+            for min_dist in kwargs['min_dist_range']:
+                embedding_indices.append(f"{n_neighbors}_{min_dist}")
+
+    # load all embeddings and add to final dict
+    all_embeddings = {}
+    for embedding_index in embedding_indices:
+        embedding_file_name = f"{embedding_dir}/{embedding_index}.z"
+        if os.path.exists(embedding_file_name):
+            all_embeddings[embedding_index] = joblib.load(embedding_file_name)
+
     # store the array of all embeddings into new file
     joblib.dump(all_embeddings, f"{embedding_dir}/all.z")
 
 
-def test_plot(method_name, n_neighbors_range):
-    for n_neighbors in n_neighbors_range:
-        in_name = f"{embedding_dir}/{n_neighbors}.z"
-        Z = joblib.load(in_name)
-        plt.figure(figsize=(8, 8))
-        plt.title(f"{dataset_name} {method_name} (n_neighbors={n_neighbors})")
-        plt.scatter(Z[:, 0], Z[:, 1], c=labels, alpha=0.4, s=14, cmap="Spectral")
-        plt.savefig(f"{plot_dir}/{n_neighbors}.png")
-        plt.close()
-
-
-def test_load_all_embeddings(method_name, list_n_neighbors=[2, 5, 20, 50]):
+def test_load_from_all_embeddings(method_name, param1=30, param2=0.1):
     all_embeddings = joblib.load(f"{embedding_dir}/all.z")
-    for n_neighbors in list_n_neighbors:
-        Z = all_embeddings[n_neighbors]
-        if Z is None:
-            continue
+    print(list(all_embeddings.keys()))
+    if method_name in ['tsne', 'largevis']:
+        embedding_index = str(param1)
+    if method_name in ['umap']:
+        embedding_index = f"{param1}_{param2}"
+    Z = all_embeddings.get(embedding_index)
 
-        plt.figure(figsize=(8, 8))
-        plt.title(f"{dataset_name} {method_name} (n_neighbors={n_neighbors})")
-        plt.scatter(Z[:, 0], Z[:, 1], c=labels, alpha=0.4, s=14, cmap="Spectral")
-        plt.savefig(f"{plot_dir}/test_load_{n_neighbors}.png")
-        plt.close()
+    plt.figure(figsize=(8, 8))
+    plt.title(f"[Debug] Test load {dataset_name} {method_name} {embedding_index}")
+    plt.scatter(Z[:, 0], Z[:, 1], c=labels, alpha=0.4, s=14, cmap="Spectral")
+    plt.savefig(f"{plot_dir}/test_load_{embedding_index}.png")
+    plt.close()
 
 
 if __name__ == "__main__":
@@ -120,8 +159,6 @@ if __name__ == "__main__":
     ap.add_argument("--run", action="store_true", help="run method for all params")
     ap.add_argument("--plot", action="store_true", help="generate plot for all params")
     args = ap.parse_args()
-
-    current_path = os.path.dirname(os.path.realpath(__file__))
 
     dataset.set_data_home("./data")
     dataset_name = args.dataset_name
@@ -138,14 +175,17 @@ if __name__ == "__main__":
             os.makedirs(dir_path)
 
     if args.debug:
-        param_range = [20]  # [2, 3, 5, 10, 15, 20, 30, 50, 100]
+        perplexity_range = [30]  # [2, 3, 5, 10, 15, 20, 30, 50, 100]
+        min_dist_range = [0.1]
     else:
-        param_range = range(2, X.shape[0] // 3)  # only for small dataset
+        perplexity_range = range(2, X.shape[0] // 3)  # only for small dataset
+        min_dist_range = [0.1]
 
     if args.run:
-        run_viz(method_name, X, param_range, seed=args.seed)
-        merge_embeddings(n_embeddings=(X.shape[0] // 3))
+        # run_viz(method_name, X, param_range, seed=args.seed)
+        merge_embeddings(method_name,
+                         perplexity_range=perplexity_range, n_neighbors_range=perplexity_range,
+                         min_dist_range=min_dist_range)
 
     if args.plot:
-        test_plot(method_name, param_range)
-        test_load_all_embeddings(method_name)
+        test_load_from_all_embeddings(method_name)
