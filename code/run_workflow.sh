8ff97379ac1ab548348c81132c773b30a65ccde6
vu-minh <vuvietminh@unamur.be>
2019-08-08 15:12:33 +0200

    add outter loop for all dataste and all methods

--- code/run_workflow.sh
+++ code/run_workflow.sh
@@ -5,27 +5,28 @@
 # workflow for reproducing
 # NOTE TO USE --seed 42
 
-
 DEFAULT_SEED=42
-DATASET_NAME=DIGITS
-METHOD=tsne
+
 
 function run_viz {
+    DATASET_NAME=$1
+    METHOD=$2
+    
     # run viz, merge all viz with selected params (in log scale) to one .z file
-    echo "RUN VIZ for ${DATASET_NAME}"
+    echo "RUN VIZ for ${DATASET_NAME} with ${METHOD}"
 
+    # for umap, run in debug mode to set min_dist to 0.1
     # for tsne and largevis, run viz for all params:
     # + set n_perp to a very large number
     # + set perp_scale to "linear"
     python run_viz.py \
 	   --seed $DEFAULT_SEED \
 	   -d $DATASET_NAME \
-	   -m $METHOD
-	   --n_perp 5000 \
+	   -m $METHOD \
 	   --perp_scale linear \
-	   --run
-
-    # for umap, run in debug mode to set min_dist to 0.1
+	   --n_perp 5000 \
+	   --run \
+	   --debug # only for umap
 }
 
 
@@ -39,6 +40,16 @@ function run_score {
     # [Debug]: From 10 =>(sim-links: 450, dis-links: 4500)
     # [Debug]: From 15 =>(sim-links: 1050, dis-links: 10125)
 
+    # COIL20
+    # [Debug]: From 3 =>(sim-links: 60, dis-links: 1710)
+    # [Debug]: From 5 =>(sim-links: 200, dis-links: 4750)
+    # [Debug]: From 10 =>(sim-links: 900, dis-links: 19000)
+    # [Debug]: From 15 =>(sim-links: 2100, dis-links: 42750)
+
+    DATASET_NAME=$1
+    METHOD=$2
+    echo "RUN SCORE for ${DATASET_NAME} with ${METHOD}"
+    
     # always run in debug mode for fixed number of labels per class (3, 5, 10, 15)
     python run_score.py \
 	   --seed $DEFAULT_SEED \
@@ -51,5 +62,17 @@ function run_score {
 }
 
 
-# run_viz
-run_score
+declare -a LIST_DATASETS=("FASHION1000") # ("DIGITS" "COIL20")
+declare -a LIST_METHODS=("tsne" "umap" "largevis")
+
+for DATASET_NAME in "${LIST_DATASETS[@]}"
+do
+    for METHOD in "${LIST_METHODS[@]}"
+    do
+	run_viz   $DATASET_NAME $METHOD
+	run_score $DATASET_NAME $METHOD
+    done
+done
+
+echo "Copy figures for latex"
+bash copy_figures_for_latex.sh
