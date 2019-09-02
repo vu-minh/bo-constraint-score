86d5e2610cd73b78909e8d916ddbf94056478aa7
vu-minh <vuvietminh@unamur.be>
2019-09-02 14:32:41 +0200

    add figure score compare for latex

--- code/copy_figures_for_latex.sh
+++ code/copy_figures_for_latex.sh
@@ -23,3 +23,8 @@ cp plots/COIL20/umap/2D/auc_rnx.png ../tex/figures/COIL20_umap_auc_rnx.png
 # (3) figures contour predicted score from BO
 cp plots/DIGITS/umap/qij/predicted_score.png ../tex/figures/DIGITS_umap_predicted_score.png
 cp plots/COIL20/umap/qij/predicted_score.png ../tex/figures/COIL20_umap_predicted_score.png
+
+
+# (4) figures comparing BIC, AUC_log_RUX and qij_score
+cp plots/DIGITS/tsne/plot_compare.png ../tex/figures/DIGITS_compare_scores.png
+cp plots/COIL20/tsne/plot_compare.png ../tex/figures/COIL20_compare_scores.png
