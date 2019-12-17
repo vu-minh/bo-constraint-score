a7fc3a5aad115ee269482c836c511cc0450d7955
vu-minh <vuvietminh@unamur.be>
2019-12-17 11:05:24 +0100

    rerun FASH_MOBILE without PCA

--- code/run_workflow.sh
+++ code/run_workflow.sh
@@ -137,25 +137,25 @@ if [ $RUN_ALL = true ]; then
     declare -a LIST_DATASETS=("FASHION1000" "DIGITS" "COIL20")
     declare -a LIST_METHODS=("tsne" "umap" "largevis")
 else
-    declare -a LIST_DATASETS=("DIGITS" "COIL20" "FASHION1000" "FASHION_MOBILENET" "NEURON_1K" "20NEWS5")
-    declare -a LIST_METHODS=("tsne" "umap")
+    declare -a LIST_DATASETS=("FASHION_MOBILENET")
+    declare -a LIST_METHODS=("tsne" "umap" "largevis")
 fi
 
 for DATASET_NAME in "${LIST_DATASETS[@]}"; do
     for METHOD in "${LIST_METHODS[@]}"; do
-	    # echo $DATASET_NAME $METHOD
-     #    if [ $METHOD == "tsne" ]; then
-     #        run_bic $DATASET_NAME # run BIC first to generate tsne embeddings
-     #    fi
-     #    run_viz $DATASET_NAME $METHOD
-	    # run_score $DATASET_NAME $METHOD
-     #    run_metric $DATASET_NAME $METHOD
+	    echo $DATASET_NAME $METHOD
+        if [ $METHOD == "tsne" ]; then
+            run_bic $DATASET_NAME # run BIC first to generate tsne embeddings
+        fi
+        run_viz $DATASET_NAME $METHOD
+	    run_score $DATASET_NAME $METHOD
+        run_metric $DATASET_NAME $METHOD
 
-     #    if [ $METHOD == "umap" ]; then
-	    #    run_score_umap_2D $DATASET_NAME
-     #    fi
+        if [ $METHOD == "umap" ]; then
+	       run_score_umap_2D $DATASET_NAME
+        fi
 
-        # plot_compare_scores $DATASET_NAME $METHOD
+        plot_compare_scores $DATASET_NAME $METHOD
         gen_metamap $DATASET_NAME $METHOD
         gen_grid_viz_demo $DATASET_NAME $METHOD
     done
